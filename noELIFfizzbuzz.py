c8db9703b4a4f990587818f289849d265849dda8
Ben McCure <502bjm@gmail.com>
2024-03-20 19:19:33 -0400

    added a version with no elif

---
+++ noELIFfizzbuzz.py
@@ -0,0 +1,9 @@
+for num in range(1, 101):
+    output = ""
+    if num % 3 == 0:
+        output += "Fizz"
+    if num % 5 == 0:
+        output += "Buzz"
+    if not output:
+        output = num
+    print(output)
